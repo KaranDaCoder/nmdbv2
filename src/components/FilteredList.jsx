7e967995e6e0280214f1a14b635baee79fb8c74c
Karan Khot <karankhot@Karans-MacBook-Air.local>
2023-09-12 22:41:41 -0500

    useContextWithWrapper

--- src/components/FilteredList.jsx
+++ src/components/FilteredList.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import React from 'react'
-import MovieCard from './MovieCard';
+// import MovieCard from './MovieCard';
 
 
 
@@ -8,9 +8,9 @@ import MovieCard from './MovieCard';
 const FilteredList = ({cat, results}) => {
   return (
     <div className='w-full min-h-screen grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-6 gap-2'>
-      {results?.map((movie,index) => (
+      {/* {results?.map((movie,index) => (
         <MovieCard movie={movie} index={index}/>
-      ))}
+      ))} */}
     
     </div>
   );
